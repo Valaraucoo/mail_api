163828c91de3e2ba3a48dfcf839011bd0929db5c
Kamil <jestem.kamil.wozniak@gmail.com>
2021-01-02 21:34:53 +0100

    feat: error handling for sending emails

--- ebin/mail_api.app
+++ ebin/mail_api.app
@@ -3,7 +3,7 @@
 	{vsn, "1.0.0"},
 	{modules, ['create_h','delete_h','info_h','list_h','mail_api_app','mail_api_sup','retrieve_h','send_h','update_h']},
 	{registered, [mail_api_sup]},
-	{applications, [kernel,stdlib,cowboy]},
+	{applications, [kernel,stdlib,gen_smtp]},
 	{mod, {mail_api_app, []}},
 	{env, []}
 ]}.
--- src/send_h.erl
+++ src/send_h.erl
@@ -2,7 +2,11 @@
 -behavior(cowboy_handler).
 
 -export([init/2]).
--export([send_email/1]).
+-export([
+	send_email/3, 
+	send_emails/2,
+	len/1
+]).
 
 
 init(Req0, State) ->
@@ -17,11 +21,18 @@ init(Req0, State) ->
             dets:close(records_db),
 
             AllEmails = lists:sort(Records),
-			[spawn(?MODULE, send_email, [binary_to_list(E)]) || {_, E} <- AllEmails],
+			send_emails(AllEmails, self()),
 
-			cowboy_req:reply(200, #{
-				<<"content-type">> => <<"application/json">>
-			}, <<"{\n\t\"message:\" \"Successfully send emails.\"\n}">>, Req0);
+			receive
+				finish ->
+					cowboy_req:reply(200, #{
+						<<"content-type">> => <<"application/json">>
+					}, <<"{\n\t\"message:\" \"Successfully send emails.\"\n}">>, Req0);
+				error ->
+					cowboy_req:reply(505, #{
+						<<"content-type">> => <<"application/json">>
+					}, <<"{\n\t\"error:\" \"Something gone wrong during sending emails.\"\n}">>, Req0)
+			end;
 		_ ->
 			cowboy_req:reply(405, #{
 				<<"content-type">> => <<"application/json">>
@@ -30,12 +41,47 @@ init(Req0, State) ->
 	{ok, Req, State}.
 
 
-send_email(Email_To) ->
+send_emails(Emails, Pid) -> 
+	N = len(Emails),
+	CounterRef = counters:new(1, [atomics]),
+
+	[spawn(?MODULE, send_email, [binary_to_list(E), self(), CounterRef]) || {_, E} <- Emails],
+
+	receive 
+		ok -> 
+			case counters:get(CounterRef, 1) of 
+				N ->
+					Pid ! finish;
+				_ -> 
+					io:fwrite("witam ~w", [counters:get(CounterRef, 1)])
+				end;
+		error -> 
+			Pid ! error
+	end, ok.
+
+
+send_email(Email_To, Pid, CounterRef) ->
 	{ok, Gmail_Login} = application:get_env(mail_api, gmail_login),
 	{ok, Gmail_Password} = application:get_env(mail_api, gmail_password),
 
-	gen_smtp_client:send({
+	Response = gen_smtp_client:send({
 		Email_To, [Email_To],	
 		"Subject: Hello\r\nFrom: Kamil Wozniak <jestem.kamil.wozniak@gmail.com>\r\nTo: \r\n\r\nYou've recived this email from Erlang Mailing API."}, 
-		[{relay, "smtp.gmail.com"}, {username, Gmail_Login}, {password, Gmail_Password}]),
+		[{relay, "smtp.gmail.com"}, {username, Gmail_Login}, {password, Gmail_Password}]
+	),
+
+	case Response of
+		{ok, _} ->
+			counters:add(CounterRef, 1, 1),
+			Pid ! ok;
+		_ -> 
+			Pid ! error
+	end,
 	ok.
+
+
+len([]) -> 0;
+len(L) -> len(L, 0).
+
+len([_|T], Count) -> len(T, Count + 1);
+len([], Count) -> Count.
